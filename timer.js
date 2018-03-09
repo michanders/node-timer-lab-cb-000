9687c086e250912196bd2e0de21477abd8b83ab6
Michael Anderson <michanders@msn.com>
2018-03-09 21:33:45 +0000

    automatically backed up by learn

---
+++ timer.js
@@ -0,0 +1,32 @@
+function timer(){
+  let seconds, minutes;
+  let [timeNum,units] = process.argv[2].split("");
+   units === "s" ? seconds = timeNum : minutes = timeNum;
+  seconds ? secondsTimer(seconds) : minutesTimer(minutes);
+
+}
+
+timer();
+
+
+function secondsTimer(seconds){
+
+  global.setInterval(function(){
+    if(seconds === 0){
+      console.log("Time is up!");
+      global.clearInterval(this);
+    }else{
+      seconds--;
+      console.log(`Left: ${seconds}s`);
+
+    }
+
+
+  }, 1000)
+}
+
+function minutesTimer(minutes){
+    let min = minutes - 1;
+    let seconds = 60;
+    secondsTimer(seconds);
+}

5d50b4c4b3b91130b4106bcc619e5e7e19feda58
Michael Anderson <michanders@msn.com>
2018-03-09 22:05:15 +0000

    automatically backed up by learn

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 function timeType(){
-  const time = process.argv[2].split("");
+  let time = process.argv[2].split("");
   var type = time[1];
   var num = parseInt(time[0]);
   type === "min" ? (num = num*60) : num;
